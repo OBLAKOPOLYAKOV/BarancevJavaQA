0a4885a74782d11edd4a106083a1ebacff6ccb5d
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-07-13 19:31:47 +0300

    Добавил возможность запускать тесты с разных браузеров
    Добавил возможность запускать тесты с разных браузеров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,18 +2,33 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.time.Duration;
 
 public class ApplicationManager {
+    private String browser;
     WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         wd.get("http://localhost/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
