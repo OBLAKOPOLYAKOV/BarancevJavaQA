cf8acf29f77fc32827edaf63426708635c33910d
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-07-25 13:03:31 +0300

    Добавил проверку на авыполнение предусловий для гурпп
    Добавил проверку на авыполнение предусловий для гурпп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().goToGroupPage();
+        if ( !app.getGroupHelper().isTheAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test2", "test1", "test3"));
