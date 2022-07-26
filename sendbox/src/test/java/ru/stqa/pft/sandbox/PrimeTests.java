ecbc20ec10539dac096f56d90ce4fee8d3b10582
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-07-26 20:52:16 +0300

    песочиница коммит

---
+++ sendbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrimes(){
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+
+}
