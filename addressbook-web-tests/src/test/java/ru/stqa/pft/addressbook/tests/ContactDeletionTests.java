3ab12b5643c1e00a76ef5ca2c6d69bbd7c31b43f
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-08-07 18:36:03 +0300

    Добавил прекондишн для теста с удалением контакта
    Добавил прекондишн для теста с удалением контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -9,6 +10,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 
 public class ContactDeletionTests extends TestBase{
+    @BeforeMethod
+    public void ensurePrecondition(){
+        app.goTo().homaPage();
+        if ( app.contact().all().size()==0){
+            app.contact().create(new ContactData()
+                    .withFirstname("Mikhail")
+                    .withLastname("Poliakov")
+                    .withAddress("Moscow, Pushkina, dom Kolotushkina")
+                    .withEmail("test@test.ru")
+                    .withBday("27")
+                    .withBmonth("September")
+                    .withByear("1996"));
+        }
+    }
     @Test
     public void testContactDeletion() {
         app.goTo().homaPage();
