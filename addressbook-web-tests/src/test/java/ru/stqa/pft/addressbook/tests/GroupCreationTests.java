f9e094db15ad54e3f62b8fca1de77c48056f6220
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-08-15 12:07:18 +0300

    add logger

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -20,6 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+    Logger logger= LoggerFactory.getLogger(GroupCreationTests.class);
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
         List<Object[]> List = new ArrayList<Object[]>();
@@ -57,6 +60,7 @@ public class GroupCreationTests extends TestBase {
 
     @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) throws Exception {
+        logger.info("Start test testGroupCreation");
         app.goTo().groupPage();
         Groups before = app.group().all();
         app.group().create(group);
@@ -64,10 +68,12 @@ public class GroupCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        logger.info("Start test testGroupCreation");
     }
 
     @Test
     public void testGroupBadCreation() throws Exception {
+        Logger.info("Start test testGroupCreation");
         app.goTo().groupPage();
         Groups before = app.group().all();
         GroupData group = new GroupData().withName("test2'");
